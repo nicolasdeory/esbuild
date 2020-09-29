wilsonzlin/esbuild-lib

add a plugin api

internal/config/config.go
@@ -1,7 +1,11 @@
 package config
 
 import (
+	"regexp"
+
+	"github.com/evanw/esbuild/internal/ast"
 	"github.com/evanw/esbuild/internal/compat"
+	"github.com/evanw/esbuild/internal/logging"
 )
 
 type LanguageTarget int8
@@ -67,6 +71,7 @@ const (
 	LoaderDataURL
 	LoaderFile
 	LoaderBinary
+	LoaderDefault
 )
 
 type Format uint8
@@ -124,6 +129,45 @@ type ExternalModules struct {
 	AbsPaths    map[string]bool
 }
 
+type ResolverPlugin struct {
+	Name     string
+	Filter   *regexp.Regexp
+	Callback func(ResolverArgs) ResolverResult
+}
+
+type ResolverArgs struct {
+	Path      string
+	ImportDir string
+}
+
+type ResolverResult struct {
+	Path      ast.Path
+	External  bool
+	Namespace string
+
+	Msgs        []logging.Msg
+	ThrownError error
+}
+
+type LoaderPlugin struct {
+	Name      string
+	Filter    *regexp.Regexp
+	Namespace string
+	Callback  func(LoaderArgs) LoaderResult
+}
+
+type LoaderArgs struct {
+	Path ast.Path
+}
+
+type LoaderResult struct {
+	Contents *string
+	Loader   Loader
+
+	Msgs        []logging.Msg
+	ThrownError error
+}
+
 type Options struct {
 	// true: imports are scanned and bundled along with the file
 	// false: imports are left alone and the file is passed through as-is
@@ -156,6 +200,9 @@ type Options struct {
 	TsConfigOverride  string
 	ExtensionToLoader map[string]Loader
 	OutputFormat      Format
+
+	ResolverPlugins []ResolverPlugin
+	LoaderPlugins   []LoaderPlugin
 
 	// If present, metadata about the bundle is written as JSON here
 	AbsMetadataFile string

nicolasdeory/esbuild

internal/config/config.go
@@ -1,7 +1,10 @@
 package config
 
 import (
+	"regexp"
+
 	"github.com/evanw/esbuild/internal/compat"
+	"github.com/evanw/esbuild/internal/logger"
 )
 
 type LanguageTarget int8
@@ -78,6 +81,7 @@ const (
 	LoaderFile
 	LoaderBinary
 	LoaderCSS
+	LoaderDefault
 )
 
 func (loader Loader) IsTypeScript() bool {
@@ -163,6 +167,48 @@ const (
 	ModeBundle
 )
 
+type ResolverPlugin struct {
+	Name      string
+	Filter    *regexp.Regexp
+	Namespace string
+	Callback  func(ResolverArgs) ResolverResult
+}
+
+type ResolverArgs struct {
+	Path       string
+	Importer   logger.Path
+	ResolveDir string
+}
+
+type ResolverResult struct {
+	Path      logger.Path
+	External  bool
+	Namespace string
+
+	Msgs        []logger.Msg
+	ThrownError error
+}
+
+type LoaderPlugin struct {
+	Name      string
+	Filter    *regexp.Regexp
+	Namespace string
+	Callback  func(LoaderArgs) LoaderResult
+}
+
+type LoaderArgs struct {
+	Path logger.Path
+}
+
+type LoaderResult struct {
+	Contents      *string
+	AbsResolveDir string
+	Loader        Loader
+
+	Msgs        []logger.Msg
+	ThrownError error
+}
+
 type Options struct {
 	Mode              Mode
 	RemoveWhitespace  bool
@@ -206,6 +252,9 @@ type Options struct {
 	TsConfigOverride  string
 	ExtensionToLoader map[string]Loader
 	OutputFormat      Format
+
+	ResolverPlugins []ResolverPlugin
+	LoaderPlugins   []LoaderPlugin
 
 	// If present, metadata about the bundle is written as JSON here
 	AbsMetadataFile string